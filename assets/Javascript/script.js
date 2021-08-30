3b4c81026c2e1f58c72507af9f01b0d745b00ecc
Eric Sun <ericmsun@gmail.com>
2021-08-30 18:33:24 -0400

    saving changes fixed bug in current weather

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -8,51 +8,45 @@ var cityHumid = document.getElementById("user-humid");
 var cityUV = document.getElementById("user-uv");
 var weatherIcon = document.getElementById("current-img");
 
-    
-
-// 5 day forecast
-// function fiveDayForecast(data) {
-    
-    
-
-    
-
-// }
 
 function weatherOutput(data) {
     var icon = data.daily[0].weather[0].icon
     console.log(icon)
     var iconUrl = `http://openweathermap.org/img/wn/` + icon + `.png`;
     weatherIcon.setAttribute('src', iconUrl);
+   
     var temperature = data.daily[0].temp.day;
     cityTemp.textContent = "Temp: " + temperature + "℉";
     console.log(temperature);
+
     var wind = data.daily[0].wind_speed;
     cityWind.textContent = "Wind: " + wind + "mph";
     console.log(wind);
+    
     var humidity = data.daily[0].humidity;
     cityHumid.textContent = "Humidity: " + humidity + "%";
     console.log(humidity);
+    
     var uvIndex = data.daily[0].uvi;
     cityUV.textContent = "UV Index: " + uvIndex;
     console.log(uvIndex);
 
-    var icon = data.daily[1].weather[0].icon
-    console.log(icon)
-    var iconUrl = `http://openweathermap.org/img/wn/` + icon + `.png`;
-    weatherIcon.setAttribute('src', iconUrl);
-    var temperature = data.daily[1].temp.day;
-    cityTemp.textContent = "Temp: " + temperature + "℉";
-    console.log(temperature);
-    var wind = data.daily[1].wind_speed;
-    cityWind.textContent = "Wind: " + wind + "mph";
-    console.log(wind);
-    var humidity = data.daily[1].humidity;
-    cityHumid.textContent = "Humidity: " + humidity + "%";
-    console.log(humidity);
-    var uvIndex = data.daily[1].uvi;
-    cityUV.textContent = "UV Index: " + uvIndex;
-    console.log(uvIndex);
+    // var icon = data.daily[1].weather[0].icon
+    // console.log(icon)
+    // var iconUrl = `http://openweathermap.org/img/wn/` + icon + `.png`;
+    // weatherIcon.setAttribute('src', iconUrl);
+    // var temperature = data.daily[1].temp.day;
+    // cityTemp.textContent = "Temp: " + temperature + "℉";
+    // console.log(temperature);
+    // var wind = data.daily[1].wind_speed;
+    // cityWind.textContent = "Wind: " + wind + "mph";
+    // console.log(wind);
+    // var humidity = data.daily[1].humidity;
+    // cityHumid.textContent = "Humidity: " + humidity + "%";
+    // console.log(humidity);
+    // var uvIndex = data.daily[1].uvi;
+    // cityUV.textContent = "UV Index: " + uvIndex;
+    // console.log(uvIndex);
     
 }
 
@@ -65,7 +59,7 @@ function showWeatherData(data) {
     var OneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=` + latitude + `&lon=` + longitude + `&exclude=minutely,hourly,alerts&units=imperial&appid=${ApiKey}`;
     
     fetch(OneCall)
-    .then(function(response) {
+    .then(function (response) {
         console.log(response)
         return response.json();
     })
@@ -73,8 +67,10 @@ function showWeatherData(data) {
         console.log(data)
         weatherOutput(data);
         // fiveDayForecast(data);
+
     })
 }
+
 
 
 searchBtn.addEventListener('click', function(event){
@@ -93,8 +89,6 @@ searchBtn.addEventListener('click', function(event){
     .catch(function(err) {
         alert("Wrong City Name!")
     })
-    
-    // fiveDayForecast();
 });
 
 
